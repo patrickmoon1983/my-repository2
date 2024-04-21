019d8705adaaaa5a391646e1d0e4dd011c0ecfe2
Patrickmoon <info4gesigner@gmail.com>
2024-04-21 21:08:05 +0300

    new change 21.04

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-(print ('Hello world'))
+import numpy as np
 lst = [x for x in range(10)]
 print(lst)
 for el in lst:
- print(el) 
+ print(el)
+res = map(lambda x: x**2, lst)
+res = list(res)
+for el in res:
+  print(el)
+
+V = np.array(res)
+print(V)
